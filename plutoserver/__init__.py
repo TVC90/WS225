198e46bd3f1dc288ad3e08f94409015616bde422
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-20 22:53:59 +0200

    Update __init__.py

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -7,7 +7,7 @@ def setup_plutoserver():
     "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
     "timeout": 60,
     "launcher_entry": {
-        "title": "Pluto.jl",
+        "title": "Recap_Proba.jl",
         "icon_path": iconpath
     },
   }
