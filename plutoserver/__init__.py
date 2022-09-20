607a6419a76ddfa38ecab41feaa69ceb0770a04a
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-20 23:30:00 +0200

    Update __init__.py

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 def setup_plutoserver():
-  mydir = os.path.dirname(os.path.realpath(../Recap_Proba.jl))
+  mydir = os.path.dirname(os.path.realpath("../Recap_Proba.jl"))
   iconpath = os.path.join(mydir, 'icons', 'pluto-logo.svg')
   return {
     "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
