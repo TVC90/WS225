69d0e26c8fffa13706b0c7aae563c771885c5acf
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-21 14:37:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 
 # read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
+this_directory = path.abspath(path.dirname("__file__"))
 
 
 setuptools.setup(
