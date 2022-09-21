da0afa6861d3b9e8d1ede7a72944c8a856184771
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-21 14:19:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="jupyter-pluto-proxy",
-    version='0.1.0',
-    url="https://github.com/illumidesk/jupyter-pluto-proxy",
-    author="IllumiDesk Team",
-    description="hello@illumidesk.com",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    keywords=['jupyter notebook', 'pluto notebook', 'jupyterhub', 'jupyterlab', 'jupyter-server-proxy'],
     classifiers=['Framework :: Jupyter'],
     install_requires=[
         'jupyter-server-proxy>=1.5.0'
