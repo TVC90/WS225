54c499a51aef3dfe9d9efdebbdcaa004db73aac2
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-21 14:40:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,5 @@ from os import path
 # read the contents of your README file
 this_directory = path.abspath(path.dirname("__file__"))
 
+pip install -e .
 
-setuptools.setup(
-    name="jupyter-pluto-proxy",
-    packages=setuptools.find_packages(),
-    classifiers=['Framework :: Jupyter'],
-    install_requires=[
-        'jupyter-server-proxy>=1.5.0'
-    ],
-    entry_points={
-        'jupyter_serverproxy_servers': [
-            'pluto = jupyter_pluto_proxy:setup_pluto',
-        ]
-    },
-    package_data={
-        'jupyter_pluto_proxy': ['icons/pluto.svg'],
-    },
-)
