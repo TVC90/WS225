0e49f8f0c6abfa9606c27456ce2c2ecca129740e
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-21 14:28:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from os import path
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setuptools.setup(
