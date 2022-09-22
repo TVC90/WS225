34b432ba9bf03419576af9a9c143456cc9904e80
init_tom <58778390+TVC90@users.noreply.github.com>
2022-09-22 08:00:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 
 setuptools.setup(
-  name="plutoserver",
-  packages=['plutoserver'],
-  package_dir={'plutoserver': 'plutoserver'},
-  package_data={'plutoserver': ['icons/pluto-logo.svg']},
+  name="jupyter-pluto-proxy",
+  py_modules=['plutoserver'],
   entry_points={
       'jupyter_serverproxy_servers': [
           # name = packagename:function_name
           'pluto = plutoserver:setup_plutoserver',
       ]
   },
-  install_requires=['jupyter-server-proxy'],
+  install_requires=['jupyter-server-proxy @ git+http://github.com/fonsp/jupyter-server-proxy@3a58aa5005f942d0c208eab9a480f6ab171142ef'],
 )
