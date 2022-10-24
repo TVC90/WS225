b79096f40d81b2520c6eb1094148019d66ad5f8f
init_tom <58778390+TVC90@users.noreply.github.com>
2022-10-24 14:21:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,10 @@ setuptools.setup(
           'pluto = plutoserver:setup_plutoserver',
       ]
   },
-  install_requires=['jupyter-server-proxy'],
+  install_requires=['jupyter-server-proxy @ git+http://github.com/fonsp/jupyter-server-proxy@3a58aa5005f942d0c208eab9a480f6ab171142ef'],
 )
+
+# because this is a demo of Pluto, we add some popular packages to the global package env and precompile
+import os
+os.system('julia -e "import Pkg; Pkg.add([\\"Markdown\\", \\"InteractiveUtils\\", \\"Documenter\\", \\"Distributions\\", \\"GLMakie\\", \\"PlutoUI\\",\\"LinearAlgebra\\"], \\"HypertextLiteral\\"); Pkg.precompile()"')
+
